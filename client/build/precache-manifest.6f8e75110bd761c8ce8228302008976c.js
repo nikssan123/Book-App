29bf369118e12f8d87f073db74617e0810137895
nikssan123 <nikssan123@gmail.com>
2020-06-24 13:41:33 +0300

    react build folder commit

---
+++ client/build/precache-manifest.6f8e75110bd761c8ce8228302008976c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109fc1fd2efb7c2599fa520fb916440c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff54928fb4c47763b362",
+    "url": "/static/css/main.e48879f7.chunk.css"
+  },
+  {
+    "revision": "42d62e9a54de72943eca",
+    "url": "/static/js/2.ce8d57e8.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.ce8d57e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff54928fb4c47763b362",
+    "url": "/static/js/main.ecdbfd2f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
